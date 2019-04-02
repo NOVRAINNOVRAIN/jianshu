5e39620773baf1c1b527c292cc0845d8a1aabcd8
supremeyh <supremeyh@126.com>
2019-04-02 18:03:06 +0800

    PropTypes

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
   constructor(props) {
@@ -6,20 +7,34 @@ class TodoItem extends Component {
     this.handleClick = this.handleClick.bind(this)
   }
 
+  // static defaultProps = {
+  //   test: 'need to do'
+  // }
+
   handleClick() {
     const {handleItemDel, index} = this.props
     handleItemDel(index)
-    // this.props.handleItemDel(this.props.index)
   }
 
   render() {
-    const { content } = this.props 
+    const { content, test } = this.props 
     return (
       <li onClick={this.handleClick}>
-        {content.text}
+        {test} -  {content.text}
       </li>
     )
   }
+}
+
+TodoItem.propTypes = {
+  text: PropTypes.string,
+  content: PropTypes.object.isRequired,
+  index: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  handleItemDel: PropTypes.func
+} 
+
+TodoItem.defaultProps = {
+  test: 're'
 }
 
 export default TodoItem
