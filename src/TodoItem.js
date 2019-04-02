9058461b06803ab2a3282c71099ace2a5d1e4dc3
supremeyh <supremeyh@126.com>
2019-04-02 18:25:45 +0800

    当组件的state或props发生改变的时候，render函数就会重新执行

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
   constructor(props) {
+    // 当组件的state或props发生改变的时候，render函数就会重新执行
+    // 当父组件的render函数被运行时，他的子组件的render都将被重新运行一次
     super(props)
     this.handleClick = this.handleClick.bind(this)
   }
-
-  // static defaultProps = {
-  //   test: 'need to do'
-  // }
 
   handleClick() {
     const {handleItemDel, index} = this.props
