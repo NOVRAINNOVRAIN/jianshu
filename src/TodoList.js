dbcb46e8f16e776e383066d11f6a3b2d346729b0
supremeyh <supremeyh@126.com>
2019-04-04 10:30:50 +0800

    createStore(reducer)

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,81 +1,26 @@
-import React, { Component, Fragment } from 'react'
-import axios from 'axios'
-import TodoItem from './TodoItem'
-import './style.css'
+import React, { Component } from 'react'
+import store from './store/index'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
 
 class TodoList extends Component {
   constructor(props) {
     super(props)
-    this.state = { inputVal: '', list: [], id: 0 }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleListItemDel = this.handleListItemDel.bind(this)
+    this.state = store.getState()
   }
 
-  handleInputChange(e) {
-    const value = e.target.value
-    this.setState(() => ({
-      inputVal: value
-    }))
-  }
-
-  handleSubmit(e) {
-    if(!this.state.inputVal) { return false }
-    this.setState((prevState, props) => ({
-      list: [...prevState.list, {text: prevState.inputVal, id: prevState.id++}],
-      inputVal: ''
-    }))
-  }
-
-  handleListItemDel(index) {
-    this.setState((prevState) => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return { list }
-    })
-  }
-
-  componentDidMount() {
-    axios.get('https://easy-mock.com/mock/5ca47d2fac5abe5a8d89b977/react/api/todolist')
-      .then((res) => {
-        this.setState(() => ({
-          list: [...res.data.data]
-        }))
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  getTodoItem() {
+  render () {
     return (
-      this.state.list.map((item, index) => {
-        return (
-          <TodoItem
-            key={item.id} 
-            handleItemDel={this.handleListItemDel}
-            content={item} 
-            index={index} />
-        )
-      })
-    )
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <h2>TodoList</h2>
-        <label htmlFor="input">input: </label>
-        <input 
-          id="input" 
-          className="input" 
-          value={this.state.inputVal} 
-          onChange={this.handleInputChange} /> 
-        <button onClick={this.handleSubmit}>submit</button>
-        <ul>
-          {this.getTodoItem()}
-        </ul>
-      </Fragment>
+      <div style={{margin: "20px 10px"}}>
+        <Input value={this.state.inputVal} placeholder="input here" size="default" style={{width: "300px", marginRight: "15px"}}/>
+        <Button type="primary">Submit</Button>
+        <List 
+          style={{width: "400px", margin: "10px 0"}}
+          bordered
+          dataSource={this.state.list}
+          renderItem={item => (<List.Item>{item}</List.Item>)}>
+        </List>
+      </div>
     )
   }
 }
---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+const defaultState = {
+  inputVal: 'react',
+  list: ['antd', 'redux', 'reducer', 'store']
+}
+
+export default (state = defaultState, action) => {
+  return state
+}
