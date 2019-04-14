b91e9b6c406d3fca3771d539198f6dc33a3f516c
supremeyh <supremeyh@126.com>
2019-04-14 15:20:24 +0800

    详情页数据redux管理

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,21 +1,24 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 import { Detailwrapper, Header, Content } from './style'
- 
+
+
 class Detail extends PureComponent {
   render() {
+    const { title, content } = this.props
     return (
       <Detailwrapper>
-        <Header>徒步香港 - 东澳古道浓浓港味之旅</Header>
-        <Content>
-          <img alt='' src='https://upload-images.jianshu.io/upload_images/5429138-82cf03e0c5ccfb8a.JPG?imageMogr2/auto-orient/strip%7CimageView2/2/w/1000/format/webp'></img>
-          <p><b>欢迎来到【薯片城市徒步计划】-香港站，我是薯片。</b></p>
-          <p>也许，你曾从香港机场起飞在3000尺高空看过太平洋，在旺角人山人海的潮鞋店里买鞋，在尖沙咀小店里吃鱼蛋，在铜锣湾的折扣季里血拼……</p>
-          <p>香港的市井生活就像一道烧腊拼盘，除了无尽的购物商场，还有许多可以值得探索的地方。与市区的水泥森林不同，香港的郊野有着绝佳的自然风光。而领略这些的最佳方式就是——徒步。</p>
-        </Content>
+        <Header>{title}</Header>
+        <Content dangerouslySetInnerHTML={{__html: content}}></Content>
       </Detailwrapper>
     )
   }
 }
 
 
-export default Detail
+const mapStateToProps = (state) => ({
+  title: state.getIn(['detail', 'title']),
+  content: state.getIn(['detail', 'content']),
+})
+
+export default connect(mapStateToProps, null)(Detail)
---
+++ src/pages/detail/store/reducer.js
@@ -0,0 +1,23 @@
+import { fromJS } from 'immutable'
+
+const content = `
+<img alt='' src='https://upload-images.jianshu.io/upload_images/5429138-82cf03e0c5ccfb8a.JPG?imageMogr2/auto-orient/strip%7CimageView2/2/w/1000/format/webp'></img>
+<p><b>欢迎来到【薯片城市徒步计划】-香港站，我是薯片。</b></p>
+<p>也许，你曾从香港机场起飞在3000尺高空看过太平洋，在旺角人山人海的潮鞋店里买鞋，在尖沙咀小店里吃鱼蛋，在铜锣湾的折扣季里血拼……</p>
+<p>香港的市井生活就像一道烧腊拼盘，除了无尽的购物商场，还有许多可以值得探索的地方。与市区的水泥森林不同，香港的郊野有着绝佳的自然风光。而领略这些的最佳方式就是——徒步。</p>
+`
+
+const defaultState = fromJS({
+  title: '徒步香港 - 东澳古道浓浓港味之旅',
+  content: content
+})
+
+const reducer = (state=defaultState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+
+export default reducer
